94d8519a6923026dc0d2399ef6d2498dd4695444
olegatronik12 <olegatronik002@gmail.com>
2023-06-09 01:27:50 +0900

    Created some returns in code

--- vk_to_discord/main.py
+++ vk_to_discord/main.py
@@ -1,7 +1,8 @@
 import discord
-import vk
+from vk import get_all
 
 
 
 
 if __name__ == '__main__':
+    print(get_all())
--- vk_to_discord/vk.py
+++ vk_to_discord/vk.py
@@ -3,7 +3,7 @@ import json
 import vk_api
 from vk_api.longpoll import VkLongPoll
 from vk_api.longpoll import VkEventType
-
+from cfg import servise_key, domain
 
 vk_session = vk_api.VkApi(app_id=51671125, token=servise_key)
 vk = vk_session.get_api()
@@ -12,19 +12,29 @@ wall = vk.wall.get(domain=domain, count=1)
 
 def get_videos(items: dict):
     list = []
+    length = len(items)
+    for number in range(length):
+        try:
+            video = f"https://vk.com/video{items[1]['video']['owner_id']}_{items[number]['video']['id']}"
+            list.append(video)
+        except:
+            pass
+    return list
+
+
+
+def get_all():
     try:
-        video = f"https://vk.com/video{attachments[3]['video']['owner_id']}_{attachments[1]['video']['id']}"
-    except:
-        pass
+        items = wall['items'][0]
+        # print(items['owner_id'], items['post_id'])
+        text = items['text']
+        text = f"`{text}`"
 
-items = wall['items'][0]
-text = items['text']
-attachments = items['attachments']
-get_videos(attachments)
-try:
-    photo = attachments[0]['photo']['sizes'][4]['url']
-    video = f"https://vk.com/video{attachments[3]['video']['owner_id']}_{attachments[1]['video']['id']}"
-    print(video)
 
-except:
-    pass
+        attachments = items['attachments']
+        url = f"https://vk.com/wall{wall['items'][-1]['owner_id']}_{wall['items'][-1]['id']}"
+        photo = attachments[0]['photo']['sizes'][4]['url']
+        videos = get_videos(attachments)
+        return url, text, videos, photo
+    except Exception as e:
+        return e
